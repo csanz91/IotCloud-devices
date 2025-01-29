57022ce3ca5da33beaed032ca6c6780fb4ff71c7
Cesar Sanz <cesarsanz.91@gmail.com>
2025-01-29 18:02:45 +0100

    Update device version to v1.0.2 and adjust temperature/humidity reading interval to 25 seconds

--- IotCloud-despacho/IotCloud-despacho.ino
+++ IotCloud-despacho/IotCloud-despacho.ino
@@ -2,7 +2,6 @@
 #include <iotcloud.h>
 
 #include "hexa_led.h"
-#include "light_sensor.h"
 #include "presence_sensor.h"
 #include "co2_sensor.h"
 #include "temp_hum_sensor.h"
@@ -14,14 +13,13 @@
 // pin for Temp/hum reading
 #define SI7021_PIN D1
 
-Device my_device = Device("v1.0", "Despacho");
+Device my_device = Device("v1.0.2", "Despacho");
 SI7021_AM2301 t_h_sensor = SI7021_AM2301(SI7021_PIN, SI7021_AM2301::SensorModel::SI7021);
 
 
 void setup(void) {
   Serial.begin(115200);
   my_device.add_sensor(new HexaLed("001", "Hexaled"));
-  my_device.add_sensor(new LIGHT_SENSOR("003_L", "Light", ""));
   my_device.add_sensor(new PRESENCE_SENSOR("002_S", "Presence", PRESENCE_PIN));
   my_device.add_sensor(new IotCloud_CO2("001_CO2", "CO2", "", MH_Z19_RX, MH_Z19_TX));
   my_device.add_sensor(new IotCloud_T_H("001_T", "Temperature", "", &t_h_sensor.temperature));
@@ -33,9 +31,9 @@ void setup(void) {
 unsigned long last_reading_T_H = 0;
 
 void loop(void) {
-  // Read temperature and humidty every 5 seconds
+  // Read temperature and humidty every 25 seconds
   unsigned long ellapsed_since_last_read_T_H = millis() - last_reading_T_H;
-  if (ellapsed_since_last_read_T_H > 5000) {
+  if (ellapsed_since_last_read_T_H > 25000) {
     t_h_sensor.read_values();
     last_reading_T_H = millis();
   }
